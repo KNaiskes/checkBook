24d1d98d577dca2d445806a5c2c12b29aec58028
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2018-11-14 21:27:26 +0200

    added cli files - nothing fully implemented for now

---
+++ src/cli.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#include "cli.h"
+
+void mainMenu()
+{
+	printf("********** Welcome to Check Book Commanl Line Interface"
+	       "**********\n");
+	printf("Enter: A to add a new record \n"
+			"D to delete a record \n"
+			"U to update/change a record\n"
+	      );
+
+}
---
+++ src/cli.h
@@ -0,0 +1,7 @@
+#ifndef CLI_H_
+#define CLI_H_
+
+void mainMenu();
+
+
+#endif // CLI_H_
