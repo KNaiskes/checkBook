8b3a1840af4a18a3cd21ddaf4f31cb5f02221e21
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2018-11-22 23:23:39 +0200

    added check to create database if it does not already exists

--- src/checkBook.c
+++ src/checkBook.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "dbFunctions.h"
 #include "cli.h"
@@ -8,6 +9,10 @@ int main(int argc, char *argv)
 {
 	extern const char *dbName;
 	dbName = "checks.db";
+
+	if(!access(dbName, F_OK) != -1) {
+		createDatabase(dbName);
+	}
 
 	mainMenu();
 
